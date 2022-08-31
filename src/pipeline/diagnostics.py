8092d476efb895f1cf7191548aa0661e6aaf6b4b
Felipe Machado <felipe.krids@gmail.com>
2022-08-31 06:46:48 -0300

    feature: creating diagnostics class

--- src/pipeline/diagnostics.py
+++ src/pipeline/diagnostics.py
@@ -1,11 +1,13 @@
 
 """
-This file is responsible for the execution of this script.
+This file is responsible for the execution of diagnostics 
+in the model and in the system process.
 
 Name: Felipe Lana Machado
 Date: 30/08/2022
 """
 
+import pickle
 import pandas as pd
 import numpy as np
 import timeit
@@ -13,22 +15,38 @@ import os
 import json
 
 from src.pipeline.pipeline import Pipeline
-from src.utils.projects_paths import CONFIG_FILE
+from src.utils.projects_paths import BASE_PATH, CONFIG_FILE
 
 class DiagnosticsPipeline(Pipeline):
 
     def __init__(self) -> None:
         super().__init__()
-        ##################Load config.json and get environment variables
         with open(CONFIG_FILE,'r') as f:
             config = json.load(f) 
 
-        self.dataset_csv_path = os.path.join(config['output_folder_path']) 
-        self.test_data_path = os.path.join(config['test_data_path']) 
+        self.dataset_csv_path = os.path.join(BASE_PATH, config['output_folder_path']) 
+        self.test_data_path = os.path.join(BASE_PATH, config['test_data_path']) 
+        self.model_path =  os.path.join(BASE_PATH, config['prod_deployment_path'], 'trainedmodel.pkl') 
 
     ##################Function to get model predictions
-    def model_predictions():
-        #read the deployed model and a test dataset, calculate predictions
+    def model_predictions(self):
+        """Read the deployed model and a test dataset, 
+            calculate predictions
+
+        Returns:
+            list: Returns a list containing all predictions
+        """     
+        data = pd.read_csv(self.data_path)
+        y = data['exited']
+        X = data.drop(['corporation', 'exited'], axis=1)
+
+        with open(self.model_path, 'rb') as f:
+            model = pickle.load(f)
+
+        y_pred = model.predict(X)
+
+        return y_pred.tolist()   
+        #
         return #return value should be a list containing all predictions
 
     ##################Function to get summary statistics
